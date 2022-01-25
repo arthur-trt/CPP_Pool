bdb8f77810ef5b1b94ef3e20e173f8ed089e1b8c
Arthur-TRT <atrouill@student.42.fr>
2022-01-25 11:19:46 +0100

    Add more test in main

--- module_05/ex00/main.cpp
+++ module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 14:27:29 by atrouill          #+#    #+#             */
-/*   Updated: 2022/01/24 14:44:00 by atrouill         ###   ########.fr       */
+/*   Updated: 2022/01/25 11:19:16 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,26 @@ int	main(void)
 	{
 		std::cerr << e.what() << '\n';
 	}
-
-
-
+	try
+	{
+		tmp.incGrade(2);
+		tmp.incGrade(5);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	std::cout << tmp << std::endl;
+	try
+	{
+		tmp2.decGrade(140);
+		tmp2.decGrade(7);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 	std::cout << tmp2 << std::endl;
+	return (0);
 }
